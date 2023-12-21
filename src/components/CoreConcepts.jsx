a0b55b8ddfdf109d81100714342a5ce0f8f2a8ef
Martin Tsindi <matefortechnology@gmail.com>
2023-12-21 11:39:33 +0000

    AddedExamplesAndCoreConcepts

---
+++ src/components/CoreConcepts.jsx
@@ -0,0 +1,22 @@
+import { CORE_CONCEPTS } from "../data.js";
+import CoreConcept from './CoreConcept';
+
+
+
+export default function CoreConcepts(){
+
+  return(
+  <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          /*key assist the data so that it does not break a unique character
+          from the object such as id must be selected to prevent warnings.*/
+          <ul>
+            {CORE_CONCEPTS.map((conceptItem) => (
+              <CoreConcept key={conceptItem.title} {...conceptItem} />
+
+            ))}
+          </ul>
+        </section>
+  )
+        
+}
